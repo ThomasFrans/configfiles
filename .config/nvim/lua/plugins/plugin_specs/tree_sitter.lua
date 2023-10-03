a44f4a867ba378b234a107332bdff4cbfe62d3a9
Thomas Frans <franske2000@gmail.com>
2023-10-03 16:22:06 +0200

    Backup

--- .config/nvim/lua/plugins/plugin_specs/tree_sitter.lua
+++ .config/nvim/lua/plugins/plugin_specs/tree_sitter.lua
@@ -36,6 +36,7 @@ return {
                 "kdl",
                 "lua",
                 "markdown",
+                "meson",
                 "php",
                 "phpdoc",
                 "ron",
